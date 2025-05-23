f195bfcd01038a227da5a6b6bd89bffd8dd8a8b5
Alan <alan.melgar.f@gmail.com>
2025-05-23 15:38:25 -0400

    atributos curso

--- app/api_model/Curso.py
+++ app/api_model/Curso.py
@@ -5,6 +5,9 @@ ns = Namespace('Curso', description='Operaciones relacionadas con curso')
 
 curso_model_request = ns.model('Curso', {
     'nombre': fields.String(required=True, description='Nombre del curso'),
+    'Paralelo': fields.String(description='A que paralelo pertenece el curso'),
+    'Turno': fields.String(description='Turno del curso (mañana, tarde o noche)'),
+    'Nivel': fields.String(description='Nivel del curso(primaria, secundaria, nidito)'),
     'descripcion': fields.String(description='Descripción del curso'),
 })
 
--- app/models/Curso_Model.py
+++ app/models/Curso_Model.py
@@ -4,6 +4,9 @@ class Curso(db.Model):
     __tablename__ = 'curso'
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(100))
+    Paralelo = db.column(db.String(255))
+    Turno = db.column(db.String(255))
+    Nivel = db.column(db.String(255))
     descripcion = db.Column(db.String(255))
 
     materias = db.relationship('MateriaCurso', back_populates='curso', lazy=True)
